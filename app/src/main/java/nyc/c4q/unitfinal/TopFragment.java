23ca333add05185fa800aa4a67f2fabe74bb60a3
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-12-20 21:32:45 -0500

    completed part03

--- app/src/main/java/nyc/c4q/unitfinal/TopFragment.java
+++ app/src/main/java/nyc/c4q/unitfinal/TopFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TopFragment extends Fragment {
+    View rootView;
+    TextView bookTitle;
+    TextView bookAuthor;
+    TextView bookYear;
 
-
+    String author;
+    String title;
+    String year;
     public TopFragment() {
         // Required empty public constructor
     }
@@ -22,8 +29,24 @@ public class TopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top, container, false);
+
+        rootView =  inflater.inflate(R.layout.fragment_top, container, false);
+        bookTitle = (TextView) rootView.findViewById(R.id.topfrag_book_title__textview);
+        bookAuthor = (TextView) rootView.findViewById(R.id.topfrag_book_author_textview);
+        bookYear = (TextView) rootView.findViewById(R.id.topfrag_book_year_textview);
+
+        return rootView;
+    }
+    public void updateView(Bundle bundle){
+
+        author = bundle.getString("author");
+        title = bundle.getString("title");
+        year = bundle.getString("year");
+        bookAuthor.setText(author);
+        bookTitle.setText(title);
+        bookYear.setText(year);
+
+
     }
 
 }
--- app/src/main/java/nyc/c4q/unitfinal/bookrecycler/view/BookViewHolder.java
+++ app/src/main/java/nyc/c4q/unitfinal/bookrecycler/view/BookViewHolder.java
@@ -1,8 +1,18 @@
 package nyc.c4q.unitfinal.bookrecycler.view;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
+import nyc.c4q.unitfinal.DisplayFragment;
+import nyc.c4q.unitfinal.R;
+import nyc.c4q.unitfinal.TopFragment;
 import nyc.c4q.unitfinal.bookrecycler.model.Book;
 
 /**
@@ -10,11 +20,52 @@ import nyc.c4q.unitfinal.bookrecycler.model.Book;
  */
 
 public class BookViewHolder extends RecyclerView.ViewHolder {
+    TextView bookTitle;
+    TextView bookAuthor;
+    TextView bookYear;
+    Activity display;
+    Context context;
+    String author;
+    String title;
+    String year;
 
-    public BookViewHolder(View itemView) {
+
+    public BookViewHolder(final View itemView) {
         super(itemView);
+        bookTitle = (TextView) itemView.findViewById(R.id.book_title_textview);
+        bookAuthor = (TextView) itemView.findViewById(R.id.book_author_textview);
+        bookYear = (TextView) itemView.findViewById(R.id.book_year_textview);
+        context = itemView.getContext();
+
+
     }
-    public void onBind(Book book){
+    public void onBind(final Book book){
+        title = book.getTitle();
+        author = book.getAuthor();
+        year = String.valueOf(book.getYear());
+        bookTitle.setText(title);
+        bookAuthor.setText(author);
+        bookYear.setText(year);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DisplayFragment displayFragment = new DisplayFragment();
+                TopFragment topFragment = (TopFragment) ((FragmentActivity) context).getSupportFragmentManager().findFragmentById(R.id.top_fragment_container_framelayout);
+
+
+                //FragmentManager fragmentManager =((FragmentActivity) context).getSupportFragmentManager();
+                //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Bundle bundle = new Bundle();
+                bundle.putString("author", author);
+                bundle.putString("title", title);
+                bundle.putString("year", year);
+                topFragment.updateView(bundle);
+
+
+
+            }
+        });
 
     }
 }
